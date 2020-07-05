64b44b6709ac6997e3a6ee0d1def2d55acbadf57
Eric Hacke <eric@roleup.io>
2020-07-05 10:46:30 -0400

    feat: works

---
+++ .asserted/example.asrtd.js
@@ -0,0 +1,61 @@
+const { expect } = require('chai');
+const io = require('socket.io-client');
+const util = require('util');
+const sinon = require('sinon');
+
+const sleep = util.promisify(setTimeout);
+
+const monitoringClient = io('http://localhost:3000', { forceNew: true });
+
+describe('socketio api tests', () => {
+  let client;
+
+  before((done) => {
+    monitoringClient.emit('add user', 'monitoring');
+    monitoringClient.once('connect', () => done());
+  });
+
+  beforeEach((done) => {
+    client = io('http://localhost:3000', { forceNew: true });
+    client.once('connect', () => done());
+  });
+
+  afterEach(() => {
+    client.disconnect();
+  });
+
+  after(() => {
+    monitoringClient.disconnect();
+  });
+
+  it('user joined and login', async () => {
+    const login = sinon.stub();
+    const joined = sinon.stub();
+
+    monitoringClient.once('user joined', joined);
+    client.once('login', login);
+
+    client.emit('add user', 'new-user');
+
+    await sleep(100);
+
+    expect(login.args).to.eql([[{ numUsers: 2 }]]);
+    expect(joined.args).to.eql([[{ numUsers: 2, username: 'new-user' }]]);
+  });
+
+  it('user sent message', async () => {
+    const monitoringMessage = sinon.stub();
+    const clientMessage = sinon.stub();
+
+    monitoringClient.once('new message', monitoringMessage);
+    client.once('new message', clientMessage);
+
+    client.emit('add user', 'new-user');
+    await sleep(100);
+    client.emit('new message', 'some-message');
+    await sleep(100);
+
+    expect(monitoringMessage.args).to.eql([[{ message: 'some-message', username: 'new-user' }]]);
+    expect(clientMessage.args).to.eql([]);
+  });
+});
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # socketio-uptime
-Test Socket IO API uptime with Asserted
+Test Socket IO uptime with Asserted
+
+![asserted.io](https://raw.githubusercontent.com/assertedio/socketio-uptime/master/images/logo.png)
+
+[Test in Prod](https://asserted.io)
+
+## Walkthough
+
+This example is referenced in a walkthrough about [SocketIO Health Checks and Uptime](https://asserted.io/posts/socketio-health-check-uptime)
+
+## Try it out
+
+```bash
+# Clone example
+git clone https://github.com/assertedio/socketio-uptime
+
+# Enter directory and install
+cd socketio-uptime/
+npm install
+
+# Run asserted tests
+npm run asrtd:test
+```
+
+## Add to your own project
+In order to create your own tests in your own repo, you should [create an account](https://app.asserted.io) and follow the quick onboarding.
+
+## Learn more
+- [Documentation](https://docs.asserted.io)
+- [asrtd CLI tool](https://github.com/assertedio/asrtd)
+
+## Notes
+
+- Typically the `asrtd` command line tool is installed globally with `npm i -g asrtd`.
+- All code related to running the tests for Asserted should be in the `.asserted` directory. 
